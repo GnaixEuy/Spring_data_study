75e766263f1e25b686918d3a897cee36399d05be
GnaixEuy <sgghcj@live.com>
2022-02-28 16:17:11 +0800

    :tada: 项目建立

---
+++ src/main/java/com/study/spring_data_study/SpringDataStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring_data_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/spring_data_study/SpringDataStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring_data_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
